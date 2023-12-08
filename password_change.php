387affdaaed42482c26a58a146fa2f7c06bed699
antoninonigro <a.nigro8@studenti.unipi.it>
2023-12-08 19:25:33 +0100

    aggiunto check agli input

--- password_change.php
+++ password_change.php
@@ -10,30 +10,73 @@
   <meta charset="UTF-8">
   <title>Change Password</title>
   <link href="CSS/stilePasswordChange.css" rel="stylesheet" type="text/css">
-	
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script>
 </head>
 <body>
   <div class="change-password-form">
-    <h2>Change Password</h2>
-    <form  action="utility/elaborate_change_password.php" method="post">
+    <h2>Cambio password</h2>
+    <form action="utility/elaborate_change_password.php" method="post" id="my_form">
     <div class="form-group">
         <label for="username">Username</label>
         <input type="text" id="username" name="username" required>
       </div>
       <div class="form-group">
-        <label for="oldPassword">Old Password</label>
+        <label for="oldPassword">Vecchia Password</label>
         <input type="password" id="old_password" name="old_password" required>
       </div>
       <div class="form-group">
-        <label for="newPassword">New Password</label>
-        <input type="password" id="new_password" name="new_password" required>
+        <label for="newPassword">Nuova Password</label>
+        <input type="password" id="new_password" name="new_password" onkeypress="controlla_sicurezza_password()" required>
+        <p id="password_strength"></p>
       </div>
       
       <div class="form-group">
-        <input type="submit" value="Change Password">
+        <input type="submit" value="Cambia Password" id="submit_button">
+        <p id="password_strength_validation"></p>
       </div>
     </form>
   </div>
+  <script>
+
+    document.getElementById("submit_button").addEventListener("click", function(event){
+        event.preventDefault();
+        const password = document.getElementById('new_password').value;
+        const result = zxcvbn(password);
+        var guesses = result.guesses_log10;
+        if(guesses < 10){
+            var p = document.getElementById("password_strength_validation");
+            p.textContent = "scrivi una password più forte";
+            p.style.color = "red";
+            p.style.fontSize = "0.8em";
+            event.preventDefault();
+            return;
+        }
+        document.getElementById("my_form").submit();
+    });
+
+    function controlla_sicurezza_password(){
+        const password = document.getElementById('new_password').value;
+        const result = zxcvbn(password);
+        var guesses = result.guesses_log10;
+        var p = document.getElementById("password_strength");
+        if(guesses <5 ){
+            p.textContent = "password debole";
+            p.style.color = "red";
+            p.style.fontSize = "0.8em";
+        }
+        else if (guesses < 10){
+            p.textContent = "password non molto forte";
+            p.style.color = "#e67014";
+            p.style.fontSize = "0.8em";
+        }
+        else{
+            p.textContent = "password forte";
+            p.style.color = "green";
+            p.style.fontSize = "0.8em";
+        }
+    }
+
+  </script>
 </body>
 </html>
 
--- utility/elaborate_change_password.php
+++ utility/elaborate_change_password.php
@@ -1,6 +1,35 @@
-<?php
-    // Connect to your database (replace these values with your actual database credentials)
-       
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Message Page</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: #f4f4f4;
+    }
+    .message-container {
+      text-align: center;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      background-color: #fff;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+  </style>
+</head>
+<body>
+  
+
+    <?php
     session_start();
     require('../inc/db.php');
 
@@ -25,7 +54,17 @@
         $row = $result->fetch_assoc();
         $stmt->bind_param("si",hash('md5',$new_password), $row["id"]);
         $stmt->execute();
-        echo "Password correctly changed";
+
+        echo"<div class='message-container' style='background: green'>";
+        echo"<h1>Operation result</h1>";
+        echo "<p>Password correttamente cambiata</p>";
+        echo "<p>torna alla main page</p>";
     }else{
-        echo "Error: retry";
+        echo"<div class='message-container' style='background: #de6666'>";
+        echo"<h1>Esito operazione</h1>";
+        echo "<p>Username non esistente o password errata, riprova</p>";
     }
+    ?>
+ </div>
+</body>
+</html>
