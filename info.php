48168e14a7ed29e17e9f0ca0c703d23cebdcefff
Tomawk <t.giorgi@studenti.unipi.it>
2023-12-10 16:54:09 +0100

    fix

--- info.php
+++ info.php
@@ -5,10 +5,13 @@
 	if(!isset($_SESSION['username'])){
 	header('location: 404.php');
 	}
-	
-	$query = "SELECT * FROM users WHERE username='".$_SESSION['username']."'";
-   	$result=mysqli_query($con,$query);
-   	$row = mysqli_fetch_assoc($result);
+
+    $stmt1 = mysqli_prepare($con,"SELECT * FROM users WHERE username = ?");
+    $stmt1->bind_param("s", $_SESSION['username']);
+    $stmt1->execute();
+    $result= $stmt1->get_result(); //only one row
+    $row = mysqli_fetch_assoc($result);
+
    	$nome = $row['nome'];
    	$cognome = $row['cognome'];
    	$email = $row['email'];
@@ -19,82 +22,35 @@
 
 ?>
 <!DOCTYPE html>
-<html lang="it">
+<html lang="eng">
 	<head>
-	<title>Informazioni Utente</title>
-	<link href="CSS/stileinfo.css" rel="stylesheet" type="text/css">
+	<title>User information</title>
+	<link href="CSS/info_style.css" rel="stylesheet" type="text/css">
 	<link rel="icon" href="immagini/key_icon.png" sizes="32x32">
+
+    <!-- Font Awesome Import -->
+    <script src="https://kit.fontawesome.com/a30f811c28.js" crossorigin="anonymous"></script>
+    <!-- Modal js include -->
+    <script src="JS/modal.js" ></script>
+
 	</head>
 <body>
-	<nav class="topnav">
- 	<table>
- 	 <tr>
-    	<td><a href="index.php">Home</a></td>
-  		<td><a href="index.php#hr1">Promozioni</a></td>
-    	<td><a href="creation.php">Ordina Online</a></td>
-    	<td><a href="index.php#hr2">Prenotazione</a></td>
-    	<td><a href="index.php#contatti">Contatti</a></td>
-
-    	<?php /* Verifica se l'utente è loggato e nel caso mostra il bottone con il nome */
-		echo '<td><a onclick="openmodal2()"><strong>'.'<i class="fas fa-user-tie"></i>'.' '. $_SESSION["username"] . '</strong></a></td>';
-		?>
-
-  		<td><a href="carrello.php">Il Mio Carrello</a></td>
-
- 	 </tr>
-	</table>
-	</nav>
+<?php include 'html/topnav.php';?>
 	<div id="center_div">
 		<div id="image">
 			<img src="immagini/user.png" alt="avatar">
 		</div>
 		<p id= "username"><strong><?php echo $_SESSION['username']?></strong></p>
-		<p id="storico"><strong>Utente dal: </strong><?php echo $date ?></p>
-		<p id= "name"><strong>Nome: </strong><?php echo $nome ?></p>
-		<p id= "cognome"><strong>Cognome: </strong><?php echo $cognome ?></p>
+		<p id="storico"><strong>User from: </strong><?php echo $date ?></p>
+		<p id= "name"><strong>Name: </strong><?php echo $nome ?></p>
+		<p id= "cognome"><strong>Surname: </strong><?php echo $cognome ?></p>
 		<p id= "email"><strong>Email: </strong><?php echo $email ?></p>
-		<p id= "indirizzo"><strong>Indirizzo: </strong><?php echo $indirizzo ?></p>
-		<p id= "citta"><strong>Citt&agrave;: </strong><?php echo $citta ?></p>
-		<p id= "cap"><strong>Cap: </strong><?php echo $cap ?></p>
+		<p id= "indirizzo"><strong>Address: </strong><?php echo $indirizzo ?></p>
+		<p id= "citta"><strong>City: </strong><?php echo $citta ?></p>
+		<p id= "cap"><strong>Postal Code: </strong><?php echo $cap ?></p>
 		<p><a id="change_pwd" href="password_change.php">Change password</a></p>
-		<!--
-		<form method="post">
-		<input type="submit" name="change" id="change" value="Modifica Profilo">
-		</form>
-		-->
-		<!-- MODAL UTENTE -->
-	</div>
-
-		<div id="id03" class="modal">
-  
-  		<div class="modal-content animate">
-    		<div class="imgcontainer">
-      		<span onclick="closemodal2()" class="close" title="Close Modal">&times;</span> <!-- Span chiusura modal -->
-      		<img src="immagini/user.png" alt="Avatar" class="avatar">
-    		</div>
-
-    		<div class="container-modal">
-      			<p><strong><?php echo $_SESSION["username"]; ?></strong></p>
-				<br>
-				<br>
-      			<button type="button" onclick="location.href = 'info.php';" class="modalbutton">Le mie informazioni</button>
-      			<button type="button" onclick="location.href = 'storico.php';" class="modalbutton">Storico Ordini</button>
-     			 <button type="button" onclick="location.href = 'utility/logout.php';" class="modalbutton">Logout</button>
-    		</div>
-
-    		<div class="container" style="background-color:#f1f1f1">
-    		</div>
-  		</div>
-		</div>
-<script> 
-	var modal3= document.getElementById('id03')
-	function openmodal2(){
-		modal3.style.display = "block";
-	}
-
-	function closemodal2(){
-		modal3.style.display = "none";
-	}
-</script>
+<?php
+include "html/modal_user.php";
+?>
 </body>
 </html>
