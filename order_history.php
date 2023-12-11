779b7353eb3dbbc4142c577397f90d18eb41aec9
Tomawk <t.giorgi@studenti.unipi.it>
2023-12-11 13:41:26 +0100

    Fix a problem in the order history related to orders with state=null

--- order_history.php
+++ order_history.php
@@ -61,22 +61,28 @@ if(!isset($_SESSION['username'])){
     $orders_ids = array_values(array_unique($orders_ids_with_duplicates));
     // get id $rows_orders[i]['id]
 
-    for($i= 0; $i<count($orders_ids); $i++){ //get how many
+    $total_orders = count($orders_ids);
+    $null_orders = 0;
+
+    for($i= 0; $i<$total_orders; $i++){ //get how many
 
         $stmt2 = mysqli_prepare($con,"SELECT * FROM ordini WHERE id = ? AND stato_ordine IS NOT NULL");
         $stmt2->bind_param("i", $orders_ids[$i]);
         $stmt2->execute();
         $result2= $stmt2->get_result(); //only one row
-        $row_order_details = mysqli_fetch_assoc($result2);
+        $result2Count=mysqli_num_rows($result2);
+        if($result2Count != 0){
 
-        $stmt3 = mysqli_prepare($con,"SELECT * FROM contenutoordini WHERE id = ?");
-        $stmt3->bind_param("i", $orders_ids[$i]);
-        $stmt3->execute();
-        $result_num_item= $stmt3->get_result();
-        $rows_num_items = mysqli_fetch_assoc($result_num_item);
-        $num_items = $rows_num_items['numero_item'];
+            $row_order_details = mysqli_fetch_assoc($result2);
 
-        echo '
+            $stmt3 = mysqli_prepare($con,"SELECT * FROM contenutoordini WHERE id = ?");
+            $stmt3->bind_param("i", $orders_ids[$i]);
+            $stmt3->execute();
+            $result_num_item= $stmt3->get_result();
+            $rows_num_items = mysqli_fetch_assoc($result_num_item);
+            $num_items = $rows_num_items['numero_item'];
+
+            echo '
    			<section>
 			<div class="image">
 				<img src="immagini/book_shipping.jpg" alt="book_shipping">
@@ -85,49 +91,49 @@ if(!isset($_SESSION['username'])){
 			<p class="quantity"> Number of items: <strong>'.$num_items.'</strong> </p>
 			<p class="shipping"> Order State:';
 
-        switch ($row_order_details['stato_ordine']) {
-            case "pending":
-                echo '
+            switch ($row_order_details['stato_ordine']) {
+                case "pending":
+                    echo '
                 <strong class="pending">'.$row_order_details['stato_ordine'].'</strong></p>
                 ';
-                break;
-            case "shipped":
-                echo '
+                    break;
+                case "shipped":
+                    echo '
                 <strong class="shipped">'.$row_order_details['stato_ordine'].'</strong></p>
                 ';
-                break;
-            case "delivered":
-                echo '
+                    break;
+                case "delivered":
+                    echo '
                 <strong class="delivered">'.$row_order_details['stato_ordine'].'</strong></p>
                 ';
-                break;
-            case "refound":
-                echo '
+                    break;
+                case "refound":
+                    echo '
                 <strong class="refound">'.$row_order_details['stato_ordine'].'</strong></p>
                 ';
-                break;
-        }
+                    break;
+            }
 
-        echo '
+            echo '
 			<p class="data"> Date of the payment: <strong>'.$row_order_details['data'].'</strong> </p>
 			<p class="total"> Total <br> &nbsp;&nbsp;<strong>'.$row_order_details['totale'].'&euro; </strong></p>
 			';
 
 
-        $stmt4 = mysqli_prepare($con,"SELECT * FROM contenutoordini WHERE id = ?");
-        $stmt4->bind_param("i", $row_order_details['id']);
-        $stmt4->execute();
-        $result3= $stmt4->get_result();
+            $stmt4 = mysqli_prepare($con,"SELECT * FROM contenutoordini WHERE id = ?");
+            $stmt4->bind_param("i", $row_order_details['id']);
+            $stmt4->execute();
+            $result3= $stmt4->get_result();
 
-        while($article_row = mysqli_fetch_assoc($result3)){
+            while($article_row = mysqli_fetch_assoc($result3)){
 
-            $stmt5 = mysqli_prepare($con,"SELECT * FROM books WHERE ISBN = ?");
-            $stmt5->bind_param("s", $article_row['ISBN']);
-            $stmt5->execute();
-            $result4= $stmt5->get_result();
-            $row_article_details = mysqli_fetch_assoc($result4);
+                $stmt5 = mysqli_prepare($con,"SELECT * FROM books WHERE ISBN = ?");
+                $stmt5->bind_param("s", $article_row['ISBN']);
+                $stmt5->execute();
+                $result4= $stmt5->get_result();
+                $row_article_details = mysqli_fetch_assoc($result4);
 
-            echo '
+                echo '
             <div class="article_box">
             <div class="book_image" style="background-image: url('.$row_article_details['image_url'].');"></div>
             <div class="book_title">'.$row_article_details['title'].'</div>
@@ -140,9 +146,19 @@ if(!isset($_SESSION['username'])){
             </div>
             <hr>
             ';
+            }
+
+            echo '</section><hr>';
+
+        } else{
+            $null_orders++;
+            if($null_orders == $total_orders){ //ha tutti ordini a null
+                echo '<h2 id="h2_empty"> Ops! There is nothing here.. </h2>
+    			  <img src="immagini/storico_empty.jpg" alt="nessun ordine" id="empty_history">
+    			  <a href="bookshelf.php" id="a_empty"> Start shopping now! </a>';
+            }
         }
 
-        echo '</section><hr>';
 
     }
 
