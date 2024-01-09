1dff0d0e6039a1d9fc85a5d2b54e3a17d966d1a8
ertento <ertento@gmail.com>
2024-01-09 16:44:43 +0100

    dfg

--- utility/sessionManager.php
+++ utility/sessionManager.php
@@ -121,8 +121,6 @@ function find_user_by_token(string $token,$con)
     function checkExpiration(){
         $last_act = $_SESSION["timestamp"];
         $now = time();
-        if($now < $last_act + 60*60  )
-        //if($now < $last_act + 5  )
         #if($now < $last_act + 60*60  )
         if($now < $last_act + 5  ){
             return true;
@@ -136,6 +134,7 @@ function find_user_by_token(string $token,$con)
             */
         }
         return false;
+}
 
 function checkSession($con)
 {
